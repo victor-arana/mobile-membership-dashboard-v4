b4dce0d49cdbca32eea4f192039da4824b1b9146
victor-arana <victor_arana@hotmail.com>
2020-09-03 22:08:08 -0500

    :cardbox: table, initialize table component

--- src/app/dashboard/table/table.component.ts
+++ src/app/dashboard/table/table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TransactionDistribution } from 'src/app/model/transaction-distribution';
 
 @Component({
   selector: 'app-table',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
-  constructor() { }
+  private transactions: TransactionDistribution[]
+
+  constructor() { 
+    this.transactions = [
+      {"parentLevel": -1, "level":0, "elementId": 1, "elementName":"General", "regularTransactions": 25, "mobileTransactions": 75},
+      {"parentLevel": 0, "level":1, "elementId": 1, "elementName":"Region 01", "regularTransactions": 20, "mobileTransactions": 50},
+      {"parentLevel": 0, "level":1, "elementId": 2, "elementName":"Region 02", "regularTransactions": 5, "mobileTransactions": 25}
+    ]
+  }
 
   ngOnInit(): void {
   }
