2ebbd4d6697361085ee5c2038dcb98613d0f4dc5
victor-arana <victor_arana@hotmail.com>
2020-09-04 18:58:08 -0500

    :wrench: material-data-table, register material data table component
    Add the MatTableModule to the app.module.ts file
    
    Changes to be committed:
    	modified:   src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { TransactionDistributionService } from './services/transaction-distribution.service';
 import { TableComponent } from './dashboard/table/table.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     TableComponent
   ],
   imports: [
+    MatTableModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
