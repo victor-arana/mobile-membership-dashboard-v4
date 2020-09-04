6b4e2e62f18ba2f8a2df58dfb70c37c23655a924
victor-arana <victor_arana@hotmail.com>
2020-09-03 19:50:25 -0500

    :wrench: service, add fetch method

--- src/app/services/transaction-distribution.service.ts
+++ src/app/services/transaction-distribution.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { TransactionDistribution } from '../model/transaction-distribution';
+import { Observable, of } from 'rxjs';
+import { delay } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +16,9 @@ export class TransactionDistributionService {
       {"parentLevel": 0, "level":1, "elementId": 1, "elementName":"Region 01", "regularTransactions": 20, "mobileTransactions": 50},
       {"parentLevel": 0, "level":1, "elementId": 2, "elementName":"Region 02", "regularTransactions": 5, "mobileTransactions": 25}
     ]
+  }
+
+  getTransactionDistribution(level, initialDate, finalDate):Observable<TransactionDistribution[]>{
+    return of(this.transactions.filter((distribution) => distribution.parentLevel == level)).pipe(delay(200));
   }
 }
