4c6faa2143208014a2ffaea2c4ece668c9d86d9f
victor-arana <victor_arana@hotmail.com>
2020-09-04 14:59:45 -0500

    :wrench: service, use service on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TransactionDistributionService } from './services/transaction-distribution.service';
+import { TransactionDistribution } from './model/transaction-distribution';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,12 @@ import { TransactionDistributionService } from './services/transaction-distribut
 export class AppComponent {
   title = 'mobile-membership-dashboard-v4';
 
+  private transactions: TransactionDistribution[];
+
   constructor(private service: TransactionDistributionService){}
+
+  ngOnInit(): void {
+    this.service.getTransactionDistribution(0,new Date(), new Date()).subscribe(d => this.transactions = d);
+  }
+
 }
