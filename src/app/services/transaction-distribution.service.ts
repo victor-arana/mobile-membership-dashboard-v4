6fa3f31237e33e1612d316d05ec7f5e522355a78
victor-arana <victor_arana@hotmail.com>
2020-09-03 19:41:08 -0500

    :wrench: service, initialize hardcoded properties

--- src/app/services/transaction-distribution.service.ts
+++ src/app/services/transaction-distribution.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { TransactionDistribution } from '../model/transaction-distribution';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionDistributionService {
 
-  constructor() { }
+  private transactions: TransactionDistribution[]
+
+  constructor() { 
+    this.transactions = [
+      {"parentLevel": -1, "level":0, "elementId": 1, "elementName":"General", "regularTransactions": 25, "mobileTransactions": 75},
+      {"parentLevel": 0, "level":1, "elementId": 1, "elementName":"Region 01", "regularTransactions": 20, "mobileTransactions": 50},
+      {"parentLevel": 0, "level":1, "elementId": 2, "elementName":"Region 02", "regularTransactions": 5, "mobileTransactions": 25}
+    ]
+  }
 }
