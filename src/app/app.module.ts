97a0e049eb7bce6e0564de87d5fcc473a006bf9d
victor-arana <victor_arana@hotmail.com>
2020-09-03 19:55:29 -0500

    :wrench: table, generates table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TransactionDistributionService } from './services/transaction-distribution.service';
+import { TableComponent } from './dashboard/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
